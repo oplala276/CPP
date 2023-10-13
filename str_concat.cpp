b812385b430fdb154b90ad65791a3d994ddcf4d2
Parag Pandharpote <9764504951s@gmail.com>
2023-10-13 12:34:29 +0530

    Added program for concatenation of two strings

---
+++ str_concat.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main(){
+    string str1, str2, str;
+
+    cin>>str1;
+    cin>>str2;
+
+    str = str1+str2;
+
+    cout<<str<<endl;
+
+    return 0;
+}
